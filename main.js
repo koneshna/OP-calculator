7d5c5c1b17fadcb5207e9dcc49154eaa2b5a100f
koneshna <guditisn@gmail.com>
2023-06-19 10:07:07 -0400

    finished calculator

--- main.js
+++ main.js
@@ -18,17 +18,33 @@ numbers.forEach((number) => number.addEventListener("click", function(e){
     handleNumber(e.target.textContent);
     current.textContent=bottomy;
         }))
+
 operators.forEach((op) => op.addEventListener("click", function(e){
     handleOperator(e.target.textContent);
     previous.textContent = topy + ' ' +operator;
     current.textContent = bottomy;
 }))
-clear.addEventListener("click", function(e){
+
+clear.addEventListener("click", function(){
     topy = '';
     bottomy = '';
     operator = '';
     previous.textContent = bottomy;
     current.textContent = topy;
+})
+
+equal.addEventListener("click", function(){
+    calculate()
+    previous.textContent = '';
+    if(topy.length <= 5){current.textContent = topy}
+    else{
+        current.textContent = topy.slice(0,5) + '...'
+    }
+    
+})
+
+deci.addEventListener("click", function(){
+    addDeci()
 })
 })
 
@@ -43,4 +59,33 @@ function handleOperator(op){
     operator = op;
     topy = bottomy;
     bottomy = '';
+}
+
+function calculate(){
+    topy = Number(topy);
+    bottomy = Number(bottomy);
+
+    if(operator === "+"){
+        topy += bottomy
+    }else if (operator === "-"){
+        topy -= bottomy;
+    }else if (operator === 'x'){
+        topy *= bottomy
+    }else {
+        topy/=bottomy
+    }
+    topy = roundNumber(topy);
+    topy=topy.toString();
+    bottomy=bottomy.toString();
+    console.log(topy);
+}
+
+function roundNumber(num){
+return Math.round(num * 1000)/1000;
+}
+
+function addDeci(){
+if (!bottomy.includes('.')){
+    bottomy += '.';
+}
 }
